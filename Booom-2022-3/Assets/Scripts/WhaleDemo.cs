2aeeb0103359e6f24e51a26059809b860c653993
坑仔 <945456973@qq.com>
2022-04-15 21:57:48 +0800

    鲸鱼

---
+++ Booom-2022-3/Assets/Scripts/WhaleDemo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class WhaleDemo : MonoBehaviour
+{
+    void Start()
+    {
+        transform.DOLocalMove(transform.localPosition + Vector3.forward * -100, 20f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            transform.GetComponent<AudioSource>().Play();
+        }
+    }
+}
