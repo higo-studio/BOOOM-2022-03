e79eff0253856317994cad47a3c0169e2ca67d81
Yang-zhicong <87217784+Yang-zhicong@users.noreply.github.com>
2022-04-16 23:47:02 +0800

    解决冲突

---
+++ Booom-2022-3/Assets/Scripts/Fish/Flock.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public float speed = 0.001f;
+    float rotationSpeed = 4.0f;
+    Vector3 averageHeading;
+    Vector3 averagePosition;
+    float neighbourDistance = 3.0f;
+
+    bool turning = false;//碰到转向
+
+    GlobalFlock globalFlock;
+
+    void Start()
+    {
+        globalFlock = GetComponentInParent<GlobalFlock>();
+        speed = Random.Range(0.5f, 1f);
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, globalFlock.transform.position) >= Vector3.Distance(globalFlock.tankSize, Vector3.zero))
+        {
+            turning = true;
+        }
+        else
+        {
+            turning = false;
+        }
+
+        if (turning)
+        {
+            Vector3 direction = globalFlock.transform.position - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                Quaternion.LookRotation(direction),
+                rotationSpeed * Time.deltaTime);
+
+            speed = Random.Range(0.5f, 1);
+        }
+        else
+        {
+            if (Random.Range(0, 5) < 1)
+            {
+                ApplyRules();
+            }
+        }
+
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+
+    void ApplyRules()
+    {
+        GameObject[] gos;
+        gos = GlobalFlock.allFish;//群组
+
+        Vector3 vcentre = Vector3.zero;//中心
+        Vector3 vavoid = Vector3.zero;//回避向量
+        float gSpeed = 0.1f;//群组速度
+
+        Vector3 goalPos = GlobalFlock.goalPos;
+        float dist;//距离
+        int groupSize = 0;
+        foreach (GameObject go in gos)
+        {
+            if (go != this.gameObject)
+            {
+                dist = Vector3.Distance(go.transform.position, this.transform.position);//距离鱼群的距离
+                if (dist <= neighbourDistance)
+                {
+                    vcentre += go.transform.position;
+                    groupSize++;
+
+                    if (dist < 1f)
+                    {
+                        vavoid += (this.transform.position - go.transform.position);
+                    }
+
+                    Flock anotherFlock = go.GetComponent<Flock>();
+                    gSpeed += anotherFlock.speed;
+
+                }
+            }
+        }
+
+        if (groupSize > 0)
+        {
+            vcentre = vcentre / groupSize + (goalPos - this.transform.position);
+            speed = Mathf.Lerp(speed, gSpeed / groupSize, Time.deltaTime * gSpeed);
+
+            Vector3 direction = (vcentre + vavoid) - transform.position;
+            if (direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation,
+                    Quaternion.LookRotation(direction),
+                    rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Booom-2022-3/Assets/Scripts/Fish/GlobalFlock.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalFlock : MonoBehaviour
+{
+    public GameObject fishPrefab;
+    public GameObject goalPrefab;
+
+    public int numFish = 10;//数量
+    public static GameObject[] allFish;
+
+    public Vector3 tankSize;//生成范围
+
+    public bool randomGoal = true;//是否随机游走
+
+
+    public static Vector3 goalPos = Vector3.zero;//目标点
+
+    void Start()
+    {
+        allFish = new GameObject[numFish];
+        for (int i = 0; i < numFish; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-tankSize.x, tankSize.x),
+                Random.Range(-tankSize.y, tankSize.y),
+                Random.Range(-tankSize.z, tankSize.z));
+            allFish[i] = (GameObject)Instantiate(fishPrefab, transform.position + pos, Quaternion.identity, this.transform);
+            allFish[i].AddComponent<Flock>();
+        }
+    }
+
+    void Update()
+    {
+        if (!randomGoal)
+        {
+            goalPos = goalPrefab.transform.position;
+        }
+        else
+        {
+            if (Random.Range(0, 10000) < 10)
+            {
+                goalPos = transform.position + new Vector3(Random.Range(-tankSize.x, tankSize.x),
+                Random.Range(-tankSize.y, tankSize.y),
+                Random.Range(-tankSize.z, tankSize.z));
+
+                goalPrefab.transform.position = goalPos;
+            }
+
+        }
+    }
+}
